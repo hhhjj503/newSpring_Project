bab44cb70b89360cacf7601a6a1ee68291087fe1
hhhjj503 <kil312055@gmail.com>
2021-01-17 21:02:41 +0900

    20210117

---
+++ webprj/src/main/java/webprj/newlecture/web/controller/Test.java
@@ -0,0 +1,12 @@
+package webprj.newlecture.web.controller;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String springVersion = org.springframework.core.SpringVersion.getVersion();
+
+		System.out.println("스프링 프레임워크 버전 : " + springVersion);
+	}
+
+}
